6a0a5deafca17bd62b0091e850362d9b459aa6f1
aleksey <aleksey@aleksey.com>
2019-01-08 23:20:14 +0300

    gg

--- MyExamProject/ModelControllers/RepositoryController.swift
+++ MyExamProject/ModelControllers/RepositoryController.swift
@@ -4,8 +4,8 @@ class RepositoryController {
     
     func getList(view: ViewController, _ completionHandler: @escaping (_ response:[Repository]) -> Void) {
         var reposList : [Repository] = []
-        let auth = "Shadow1702,0ebc055e7a829054e448ffe500639073105eef90"
-        JsonAction.newGet(address: "https://api.github.com/users/Shadow1702/repos", requestHeaders: ["User-Agent" : "Awesome-Octocat-App", "authorization" : auth.toBase64()], view: view, {response in
+        let auth = "EclipseMops:dasdasr52452"
+        JsonAction.newGet(address: "https://api.github.com/user/repos", requestHeaders: ["User-Agent" : "Awesome-Octocat-App", "authorization" : "Basic " + auth.toBase64()], view: view, {response in
             if let json = response?.value as? [Any] {
                 for _rep in json {
                     if let rep = _rep as? [String : Any] {
